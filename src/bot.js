19a947991b47fa351893593ac14cb4ce0098c155
Adinkkk725 <121096995+Adinkkk725@users.noreply.github.com>
2025-02-15 02:34:11 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,23 +1,22 @@
 const { keepWebSocketAlive } = require('./tasks');
-const chalk = require('chalk');
 
 function displayBanner() {
-  console.log(chalk.blue(`
+  console.log(`
    ___      _       _     
   / _ \\__ _| |_ ___| |__  
  / /_)/ _\` | __/ __| '_ \\ 
 / ___/ (_| | || (__| | | |
 \\/    \\__,_|\\__\\___|_| |_|
                           
-`));
+`);
 }
 
 (async () => {
   displayBanner();
-  console.log(chalk.cyan('Starting SparkChain auto bot...'));
+  console.log('Starting SparkChain auto bot...');
 
   // Keep WebSocket connection alive
   await keepWebSocketAlive();
 
-  console.log(chalk.cyan('SparkChain auto bot finished.'));
+  console.log('SparkChain auto bot finished.');
 })();
