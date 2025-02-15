ca0fa65f7a1df6eb20e63652b26aee95cd3e94ad
Adinkkk725 <121096995+Adinkkk725@users.noreply.github.com>
2025-02-15 13:47:32 +0800

    Update tasks.js

--- src/tasks.js
+++ src/tasks.js
@@ -1,27 +1,4 @@
 const WebSocket = require('ws');
-const axios = require('axios');
-
-const API_URL = 'https://api.depined.org/api/user/widget-connect';
-
-async function connectToApi(token) {
-  try {
-    const response = await axios.post(API_URL, {
-      // Data yang diperlukan untuk permintaan POST
-    }, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
-    console.log('Connected to API:', response.data);
-  } catch (error) {
-    console.error('Error connecting to API:', error);
-    if (error.response) {
-      console.error('Status Code:', error.response.status);
-      console.error('Status Text:', error.response.statusText);
-      console.error('Response Data:', error.response.data);
-    }
-  }
-}
 
 async function keepWebSocketAlive(token) {
   const url = `wss://ws-v2.sparkchain.ai/socket.io/?token=${token}&device_id=77aa1b6c-70bd-42f0-bbd4-e8d0a9702964&device_version=0.7.0&EIO=4&transport=websocket`;
@@ -32,9 +9,9 @@ async function keepWebSocketAlive(token) {
   });
 
   ws.on('message', (message) => {
+    console.log('Received message:', message);
     try {
       const messageString = message.toString('utf-8');
-      console.log('Received message:', messageString);
       if (messageString.startsWith('{') || messageString.startsWith('[')) {
         const data = JSON.parse(messageString);
         if (data && data.points) {
@@ -64,4 +41,4 @@ async function keepWebSocketAlive(token) {
   });
 }
 
-module.exports = { keepWebSocketAlive, connectToApi };
+module.exports = { keepWebSocketAlive };
